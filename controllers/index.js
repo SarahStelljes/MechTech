8e9dd7721627a9ef9ec20b590acf97e8e63cdee1
Battlehart <sarah.stelljes@gmail.com>
2022-09-15 08:57:41 -0400

    created home routes, edited comment routes and post routes a tad. Created utils/helpers.js, views handlebar files and folders, and edited server.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRutes);
 
 router.use((req, res) => {
     res.status(404).end();
